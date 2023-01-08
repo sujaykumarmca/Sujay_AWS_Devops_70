c500b86174a34a4a913bfbab54c5d52f0327cfdc
sujay <bsujaykumar@live.com>
2023-01-08 12:06:41 +0530

    Declared variable and printing the variable.

--- Shell_Scripting_Learn/01_comments.sh
+++ Shell_Scripting_Learn/01_comments.sh
@@ -3,10 +3,18 @@ echo "Hello World"
 car="swift1"
 echo "Car Name is::"+$car
 echo "****Hello World*******"
+
+
 a=20
 b=30
 total=$((a+b))
 echo "Total is::"+$total
+
+#readonly variables
+c readonly 30;
+c=40
+echo "C="+c
+
 
 #printing dates
 today_date=$(date +%F)
@@ -14,7 +22,9 @@ echo "Hey Todays date is::+$today_date"
 
 #declaring arrays in shell script
 i=(10 20 30)
-echo "Printing Array is $i{[0]}"
-echo "Printing Array is $i{[1]}"
-echo "Printing Array is $i{[2]}"
+echo "Printing Array  ${i[0]}"
+echo "Printing Array  ${i[1]}"
+echo "Printing Array  ${i[2]}"
+
+
 
