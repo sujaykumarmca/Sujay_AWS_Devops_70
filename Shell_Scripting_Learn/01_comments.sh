6c9fc255ce93836da4a9c4aa6c76c9057577ab95
sujay <bsujaykumar@live.com>
2023-01-08 12:02:30 +0530

    Declared variable and printing the variable.

--- Shell_Scripting_Learn/01_comments.sh
+++ Shell_Scripting_Learn/01_comments.sh
@@ -11,3 +11,10 @@ echo "Total is::"+$total
 #printing dates
 today_date=$(date +%F)
 echo "Hey Todays date is::+$today_date"
+
+#declaring arrays in shell script
+i=(10 20 30)
+echo "Printing Array is $i{[0]}"
+echo "Printing Array is $i{[1]}"
+echo "Printing Array is $i{[2]}"
+
